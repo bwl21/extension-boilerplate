78b491348e15e90ba19b184ed70f3ca425c5fd62
Joscha Ziegeler <jziegeler@churchtools.de>
2025-09-02 17:33:23 +0200

    first files for boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# plugin-boilerplate
+# Building ChurchTools-Plugins
+
+With this project you get a boilerplate for building your own plugin for [ChurchTools](https://www.church.tools).
+
+## Configuration
+
+In the `.env` you can configure the necessary constants to start with your project. With that, the ChurchTools-Client is ready to go and you can start using the ChurchTools-API. **Make sure you do include it in `.gitignore`!**
+
+So: Happy coding
+
+For questions concerning the API reach out in the [Forum](https://forum.church.tools).
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import type { Person } from './ct-types';
+import './style.css';
+import { churchtoolsClient } from '@churchtools/churchtools-client';
+
+declare const window: Window &
+    typeof globalThis & {
+        settings: {
+            base_url?: string;
+        };
+    };
+
+const baseUrl = window.settings?.base_url ?? import.meta.env.VITE_BASE_URL;
+churchtoolsClient.setBaseUrl(baseUrl);
+
+const username = import.meta.env.VITE_USERNAME;
+const password = import.meta.env.VITE_PASSWORD;
+if (import.meta.env.MODE === 'development' && username && password) {
+    await churchtoolsClient.post('/login', { username, password });
+}
+
+const KEY = import.meta.env.VITE_KEY;
+export { KEY };
+
+const user = await churchtoolsClient.get<Person>(`/whoami`);
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Welcome ${[user.firstName, user.lastName].join(' ')}</h1>
+  </div>
+`;
